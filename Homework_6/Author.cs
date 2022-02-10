791da1721b4edbe5c328147c37ead0e19efb04d0
Artem Mochuliak <temasgo@gmail.com>
2022-02-10 16:39:15 +0100

    Homework_6(Artem Mochuliak)

---
+++ Homework_6/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6
+{
+    internal class Author
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Birthday { get; set; }
+        public string Nationality { get; set; }
+       
+    }
+}
---
+++ Homework_6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Author author = new Author();
+            author.Name = "John";
+            author.Surname = "Bowie";
+            author.Birthday = 1967;
+            author.Nationality = "englishman";
+            Book book = new Book();
+            book.Id = 503843;
+            book.Title = "Murder";
+            book.Genre = "Criminal";
+            book.Quantity_Of_Pages = 234;
+            Publisher publisher = new Publisher("RELX", 1999,"England");
+        
+        }
+    }
+}
